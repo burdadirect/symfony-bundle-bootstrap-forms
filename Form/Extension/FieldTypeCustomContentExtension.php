a9b6209222b65684db0fc68bdf55fa7bab1d4e54
Christian Puchinger <puchinger@playboy.de>
2019-04-29 20:07:17 +0200

    Add custom content extension.

---
+++ Form/Extension/FieldTypeCustomContentExtension.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace HBM\BootstrapFormBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FieldTypeCustomContentExtension extends AbstractTypeExtension {
+
+  /**
+   * @param FormBuilderInterface $builder
+   * @param array                $options
+   */
+  public function buildForm(FormBuilderInterface $builder, array $options) {
+    if (isset($options['custom_content'])) {
+      $builder->setAttribute('custom_content', $options['custom_content']);
+    }
+  }
+
+  /**
+   * @param FormView      $view
+   * @param FormInterface $form
+   * @param array         $options
+   */
+  public function buildView(FormView $view, FormInterface $form, array $options) {
+    if (isset($options['custom_content'])) {
+      $view->vars['custom_content'] = $options['custom_content'];
+    }
+  }
+
+  /**
+   * Add the custom_content option
+   *
+   * @param OptionsResolver $resolver
+   */
+  public function configureOptions(OptionsResolver $resolver) {
+    $resolver->setDefined(['custom_content']);
+  }
+
+  /**
+   * @return array
+   */
+  public static function getExtendedTypes() : iterable {
+    return [FormType::class];
+  }
+
+}
